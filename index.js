2578dc5f0c7cc257676be4848f49dafbd137957e
Remigiusz Ambroziak <remigiusz.ambroziak@schibsted.pl>
2017-03-24 11:43:28 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+module.exports = {
+  getGlobals,
+  printGlobals
+};
+
+
+function getGlobals() {
+  const forceExclude = [
+    'getGlobals',
+    'forceExclude',
+    'runnerWindow',
+    'iWindow',
+    'freshContext',
+    'globals'
+  ];
+  
+  const iWindow = document.createElement('iframe');
+  document.body.appendChild(iWindow);
+  const freshContext = iWindow.contentWindow;
+
+  const globals = Object
+    .keys(window)
+    .filter(key => !freshContext.hasOwnProperty(key))
+    .filter(key => !forceExclude.includes(key));
+
+  document.body.removeChild(iWindow);
+
+  return globals;
+};
+
+function printGlobals() {
+  const globals = getGlobals().reduce((globals, globalVar) => {
+    globals += (' ' + globalVar);
+    return globals;
+  }, 'Available global variables: ');
+
+  console.log(globals);
+};
